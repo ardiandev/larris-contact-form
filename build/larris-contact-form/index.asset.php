0c936b1c70702883af604a5edd9bd1dda6f4458a
Ardian Pradana <ardianysp18@gmail.com>
2025-04-07 23:24:47 +0700

    WIP: temporary changes

--- build/larris-contact-form/index.asset.php
+++ build/larris-contact-form/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0d8b2fb1bf56a7830121');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2cd88938c4c6ed15c4b9');
